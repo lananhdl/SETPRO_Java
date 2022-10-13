2ca6cea357032bb31173a1d01786294ca687d06f
Alana Nguyen <alana.nguyen@beyondedge.com.sg>
2022-10-13 11:59:54 +0700

    lab 10 done

--- lession10/AnimalTest.java
+++ lession10/AnimalTest.java
@@ -1,4 +1,5 @@
 package lession10;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -11,14 +12,36 @@ public class AnimalTest {
     }
 
     public void animalWinner(List<Animal> animalList) {
+        ArrayList<Integer> animalSpeedList = new ArrayList<>();
+        ArrayList<String> animalNameList = new ArrayList<>();
         Animal winner = animalList.get(0);
-
         for (Animal animals : animalList) {
-                if (animals.getSpeed() > winner.getSpeed()) {
-                    winner = animals;
-            }
-            System.out.println(animals.getName() +" : "+ animals.getSpeed());
+            String name= animals.getName();
+            int speed= animals.getSpeed();
+            animalSpeedList.add(speed);
+            animalNameList.add(name);
+            System.out.println(name + " : "+ speed);
         }
-        System.out.println("Winner is: "+winner.getName()+(" with ") + winner.getSpeed() + " km/h");
+        int maxValue = 0;
+        int index = 0;
+        int indexAnimal=0;
+        for (int valueSpeed : animalSpeedList
+        ) {
+
+            if (valueSpeed > maxValue) {
+                maxValue = valueSpeed;
+                index++;
+            }
+
+        }
+        for (String winnerName:animalNameList
+        ) {  indexAnimal++;
+            if(indexAnimal==index){
+                System.out.println("Winner is: "+ winnerName +" with speed is: "+ maxValue );
+            }
+
+        }
+
     }
 }
+
